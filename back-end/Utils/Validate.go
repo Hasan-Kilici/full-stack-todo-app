31c259cfce5728e234e44a3478b8cee263022710
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2023-08-04 12:20:56 +0300

    Update Validate.go

--- back-end/Utils/Validate.go
+++ back-end/Utils/Validate.go
@@ -1,1 +1,17 @@
 package Utils
+
+import(
+	"fmt"
+	"strings"
+)
+
+func ValidateTask(Task string) error {
+	if len(Task) < 5 {
+		return fmt.Errorf("Task cannot be less than 5 characters")
+	}
+
+	if strings.HasPrefix(Task, " ") {
+		return fmt.Errorf("Task cannot start with a space")
+	}
+	return nil
+}
